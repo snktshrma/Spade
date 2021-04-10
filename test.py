b54d67a2922ed4bcad9357da290ca820bd71961e
ezzatisawesome <ezzatisawesome@gmail.com>
2021-04-10 15:30:10 -0500

    chnage

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
 from transformers import pipeline
 
+def questionAnswer(context, question):
+    question_answering = pipeline("question-answering")
 
-question_answering = pipeline("question-answering")
+    result = question_answering(question=question, context=context)
 
-context = """
-Machine learning (ML) is the study of computer algorithms that improve automatically through experience. It is seen as a part of artificial intelligence. Machine learning algorithms build a model based on sample data, known as "training data", in order to make predictions or decisions without being explicitly programmed to do so. Machine learning algorithms are used in a wide variety of applications, such as email filtering and computer vision, where it is difficult or unfeasible to develop conventional algorithms to perform the needed tasks.
-"""
+    answer = result['answer']
+    score = result['score']
+    
+    return score, answer
 
-question = "What are machine learning models based on?"
 
-result = question_answering(question=question, context=context)
-print("Answer:", result['answer'])
-print("Score:", result['score'])
+if __name__ == "__main__":
+
+    context = """
+    Machine learning (ML) is the study of computer algorithms that improve automatically through experience. It is seen as a part of artificial intelligence. Machine learning algorithms build a model based on sample data, known as "training data", in order to make predictions or decisions without being explicitly programmed to do so. Machine learning algorithms are used in a wide variety of applications, such as email filtering and computer vision, where it is difficult or unfeasible to develop conventional algorithms to perform the needed tasks.
+    """
+    question = "What are machine learning models based on?"
+
+    score, answer = questionAnswer(context, question)
+
+    print("Answer: ", answer)
+    print("Score: ", score)
