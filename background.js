d9c5c3e5191785ab8c5b7dde06caf9bf36665be6
titut <conghoangbillnew@gmail.com>
2021-04-09 21:49:48 -0500

    Drawing

--- background.js
+++ background.js
@@ -1,10 +1,32 @@
-var state = true;
+var state = null;
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    if(confirm("Delete Data?")){
-        chrome.storage.local.clear();
+chrome.storage.local.get(["startStop"], function(result){
+    if(result.startStop == undefined){
+        state = true;
+    } else {
+        state = result.startStop;
     }
+    if(state){
+        document.getElementById("start").innerHTML = "Start";
+    } else {
+        document.getElementById("start").innerHTML = "Stop";
+    }
+})
+
+
+document.getElementById("start").addEventListener("click", function(){ 
     chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
-        chrome.tabs.sendMessage(tabs[0].id, {greeting: "HELLO"})
+        chrome.tabs.sendMessage(tabs[0].id, {greeting: state})
+        chrome.storage.local.set({startStop: !state});
+        state = !state;
+        if(state){
+            document.getElementById("start").innerHTML = "Start";
+        } else {
+            document.getElementById("start").innerHTML = "Stop";
+        }
     })
-});
+})
+
+document.getElementById("erase").addEventListener("click", function(){
+    chrome.storage.local.remove(['data']);
+})
--- main.js
+++ main.js
@@ -3,46 +3,56 @@ var page = {
     "strokes": []
 }
 
-chrome.runtime.onMessage.addListener(function(req, send, res){   
-    
+chrome.runtime.onMessage.addListener(function(req, send, res){
+    if(req.greeting){
+        startSketch();
+    } else {
+        let elem = document.getElementById("defaultCanvas0");
+        elem.parentNode.removeChild(elem);
+    }
 })
 
-function setup(){
-    var c = createCanvas(document.body.offsetWidth, document.body.offsetHeight);
-    c.position(0, 0);
-    clear();
-    stroke(0);
-    strokeWeight(4);
-    chrome.storage.local.get(['data'], function(result){
-        let index = checkArray(result.data);
-        if(!(index == -1)){
-            for(let strokes of result.data[index].strokes){
-                page.strokes.push(strokes);
-                line(strokes.mouseX, strokes.mouseY, strokes.pmouseX, strokes.pmouseY);
+function startSketch(){
+    const s = (sketch) =>{
+        sketch.setup = ()=>{
+            var c = sketch.createCanvas(document.body.offsetWidth, document.body.offsetHeight);
+            c.position(0, 0);
+            sketch.clear();
+            sketch.stroke(0);
+            sketch.strokeWeight(4);
+            chrome.storage.local.get(['data'], function(result){
+                let index = checkArray(result.data);
+                if(!(index == -1)){
+                    for(let strokes of result.data[index].strokes){
+                        page.strokes.push(strokes);
+                        sketch.line(strokes.mouseX, strokes.mouseY, strokes.pmouseX, strokes.pmouseY);
+                    }
+                }
+            })
+        }
+
+        sketch.draw = ()=>{
+            sketch.stroke(0);
+            sketch.strokeWeight(4);
+            if(sketch.mouseIsPressed){
+                sketch.line(sketch.mouseX,sketch.mouseY,sketch.pmouseX,sketch.pmouseY);
+                page["strokes"].push({
+                    "mouseX": sketch.mouseX,
+                    "mouseY": sketch.mouseY,
+                    "pmouseX": sketch.pmouseX,
+                    "pmouseY": sketch.pmouseY
+                });
+                saveData()
             }
         }
-    })
-}
 
-function draw(){
-    stroke(0);
-    strokeWeight(4);
-    if(mouseIsPressed){
-        line(mouseX,mouseY,pmouseX,pmouseY);
-        page["strokes"].push({
-            "mouseX": mouseX,
-            "mouseY": mouseY,
-            "pmouseX": pmouseX,
-            "pmouseY": pmouseY
-        });
-        saveData()
+        sketch.keyPressed = ()=>{
+            if(sketch.keyCode == 87){
+                chrome.storage.local.clear();
+            }
+        }
     }
-}
-
-function keyPressed(){
-    if(keyCode == 87){
-        remove();
-    }
+    let myp5 = new p5(s);
 }
 
 function checkArray(array){
