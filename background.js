80f60e7bcaba0d549a5f96060a3922bfe0dfbdac
titut <conghoangbillnew@gmail.com>
2021-04-09 22:53:46 -0500

    clear button

--- background.js
+++ background.js
@@ -38,6 +38,12 @@ document.getElementById("highlight").addEventListener("click", function(){
     })
 })
 
-document.getElementById("erase").addEventListener("click", function(){
+document.getElementById("clear").addEventListener("click", function(){
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id, {greeting: false})
+        chrome.storage.local.set({startStop: true});
+        state = true
+        document.getElementById("start").innerHTML = "Start";
+    })
     chrome.storage.local.remove(['data']);
 })
