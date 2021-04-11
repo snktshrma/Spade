bc006533fcfefb9bbd1d4a5b10ef694d4488cdb2
ezzatisawesome <ezzatisawesome@gmail.com>
2021-04-10 23:22:46 -0500

    new

--- Spade/test.py
+++ Spade/test.py
@@ -10,13 +10,12 @@ def questionAnswer(context, question):
     
     return score, answer
 
-
 if __name__ == "__main__":
 
     context = """
-    Machine learning (ML) is the study of computer algorithms that improve automatically through experience. It is seen as a part of artificial intelligence. Machine learning algorithms build a model based on sample data, known as "training data", in order to make predictions or decisions without being explicitly programmed to do so. Machine learning algorithms are used in a wide variety of applications, such as email filtering and computer vision, where it is difficult or unfeasible to develop conventional algorithms to perform the needed tasks.
+    The founder of LFA is Mr. Dejesus.
     """
-    question = "What are machine learning models based on?"
+    question = "Who is the founder of LFA?"
 
     score, answer = questionAnswer(context, question)
 
